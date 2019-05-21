383c14f9e3ef644160cd124a5c1a96f66f8aa3c8
pingouinfini <pingouinfini@gmail.com>
2019-05-21 15:15:01 +0200

    add docker files

---
+++ docker/compose/reach.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+if [ $# -lt 1 ]; then
+  echo "Usage: reach.sh VERB"
+  echo
+  echo VERB must be one of:
+  echo "  • install"
+  echo "  • run "
+  echo "  • stop "
+  echo
+  echo "Examples:"
+  echo "  • $0 install"
+  echo "  • $0 run"
+  exit 1
+fi
+
+verb=$1
+case $verb in
+run|install|stop)
+  ;;
+*)
+  echo "error: unknown verb '$verb'"
+  exit 1
+esac
+
+case $verb in
+
+install)
+echo "stopping insight if running"
+docker-compose -f reach.yml down
+echo "installing reach .. "
+#create directory /files OR execute preparation scripts BELOW, before build
+docker-compose -f reach.yml -p reach up -d --build
+;;
+
+run)
+
+echo "starting reach .. "
+docker-compose -f reach.yml -p reach up -d
+;;
+
+stop)
+
+echo "stopping reach .. "
+docker-compose -f reach.yml -p reach down
+;;
+esac
